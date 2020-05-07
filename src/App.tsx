fbb2a9956b6c6a506a3f13082a8fec8016a55b97
Steve Reid <steve@appby.design>
2020-05-07 13:23:59 +0100

    init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Form } from 'react-formio';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,6 +14,7 @@ function App() {
         >
           Learn React
         </a>
+        <Form src="https://ufyrebemhtafezz.form.io/user/login" />
       </header>
     </div>
   );
